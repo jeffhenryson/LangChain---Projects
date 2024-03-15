797e2b6e015fa1fe7aee60138c5ebf79254a0afc
jeffhenryson <jeffhunbruey@gmail.com>
2024-03-15 17:44:42 -0300

    Projetos em LangChain - AI

---
+++ Chat_with_Langchain/app.py
@@ -0,0 +1,171 @@
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+
+load_dotenv()
+
+def get_vectorstores_from_urls(urls):
+    
+    all_document_chunks = []
+    for url in urls:
+        loader = WebBaseLoader(url)
+        document = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter()
+        document_chunks = text_splitter.split_documents(document)
+        all_document_chunks.extend(document_chunks)
+
+    combined_vector_store = Chroma.from_documents(all_document_chunks, OpenAIEmbeddings())
+
+    return combined_vector_store
+
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatOpenAI(temperature=0.8)
+    
+    retriever = vector_store.as_retriever()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    
+    return retriever_chain
+    
+def get_conversational_rag_chain(retriever_chain): 
+    
+    llm = ChatOpenAI()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    
+    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
+    
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+def get_response(user_input):
+    
+    if "vector_store" not in st.session_state or st.session_state.vector_store is None:
+        return "No vector store available."
+
+    vector_store = st.session_state.vector_store
+    retriever_chain = get_context_retriever_chain(vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_input
+    })
+
+    return response['answer']
+
+# app config
+st.set_page_config(page_title="Chat with websites", page_icon="🤖")
+st.title("Chat with websites")
+
+# sidebar
+with st.sidebar:
+    st.header("Settings")
+    website_urls_input = """
+    https://python.langchain.com/docs/get_started/introduction,
+    https://python.langchain.com/docs/get_started/installation,
+    https://python.langchain.com/docs/expression_language/get_started,
+    https://python.langchain.com/docs/expression_language/why,
+    https://python.langchain.com/docs/expression_language/interface,
+    https://python.langchain.com/docs/expression_language/streaming,
+    https://python.langchain.com/docs/expression_language/how_to/map,
+    https://python.langchain.com/docs/expression_language/how_to/passthrough,
+    https://python.langchain.com/docs/expression_language/how_to/functions,
+    https://python.langchain.com/docs/expression_language/how_to/routing,
+    https://python.langchain.com/docs/expression_language/how_to/binding,
+    https://python.langchain.com/docs/expression_language/how_to/configure,
+    https://python.langchain.com/docs/expression_language/how_to/decorator,
+    https://python.langchain.com/docs/expression_language/how_to/fallbacks,
+    https://python.langchain.com/docs/expression_language/how_to/generators,
+    https://python.langchain.com/docs/expression_language/how_to/inspect,
+    https://python.langchain.com/docs/expression_language/how_to/message_history,
+    https://python.langchain.com/docs/expression_language/cookbook/prompt_llm_parser,
+    https://python.langchain.com/docs/expression_language/cookbook/retrieval,
+    https://python.langchain.com/docs/expression_language/cookbook/multiple_chains,
+    https://python.langchain.com/docs/expression_language/cookbook/sql_db,
+    https://python.langchain.com/docs/expression_language/cookbook/agent,
+    https://python.langchain.com/docs/expression_language/cookbook/code_writing,
+    https://python.langchain.com/docs/expression_language/cookbook/embedding_router,
+    https://python.langchain.com/docs/expression_language/cookbook/memory,
+    https://python.langchain.com/docs/expression_language/cookbook/moderation,
+    https://python.langchain.com/docs/expression_language/cookbook/prompt_size,
+    https://python.langchain.com/docs/expression_language/cookbook/tools,
+    https://python.langchain.com/docs/modules/,
+    https://python.langchain.com/docs/modules/model_io/,
+    https://python.langchain.com/docs/modules/model_io/concepts,
+    https://python.langchain.com/docs/modules/model_io/prompts/composition,
+    https://python.langchain.com/docs/modules/model_io/prompts/few_shot_examples,
+    https://python.langchain.com/docs/modules/model_io/prompts/few_shot_examples_chat,
+    https://python.langchain.com/docs/modules/model_io/prompts/message_prompts,
+    https://python.langchain.com/docs/modules/model_io/prompts/partial,
+    https://python.langchain.com/docs/modules/model_io/prompts/pipeline,
+    https://python.langchain.com/docs/modules/model_io/chat/quick_start,
+    https://python.langchain.com/docs/modules/model_io/chat/function_calling,
+    https://python.langchain.com/docs/modules/model_io/chat/chat_model_caching,
+    https://python.langchain.com/docs/modules/model_io/chat/custom_chat_model,
+    https://python.langchain.com/docs/modules/model_io/chat/logprobs,
+    https://python.langchain.com/docs/modules/model_io/chat/streaming,
+    https://python.langchain.com/docs/modules/model_io/llms/custom_llm,
+    https://python.langchain.com/docs/modules/model_io/llms/llm_caching,
+    https://python.langchain.com/docs/modules/model_io/llms/streaming_llm,
+    https://python.langchain.com/docs/modules/model_io/llms/token_usage_tracking,
+    https://python.langchain.com/docs/modules/model_io/output_parsers/quick_start,
+    https://python.langchain.com/docs/modules/model_io/output_parsers/types/csv,
+    https://python.langchain.com/docs/modules/model_io/output_parsers/types/json,
+    https://python.langchain.com/docs/modules/model_io/output_parsers/types/openai_functions,
+    https://python.langchain.com/docs/modules/model_io/output_parsers/types/openai_tools,
+    https://python.langchain.com/docs/modules/model_io/output_parsers/types/pandas_dataframe,
+    https://python.langchain.com/docs/modules/model_io/output_parsers/types/pydantic,
+    https://python.langchain.com/docs/modules/model_io/output_parsers/types/retry,
+    https://python.langchain.com/docs/modules/model_io/output_parsers/types/structured,
+    
+    """
+    website_urls = [url.strip() for url in website_urls_input.split(",") if url.strip()]
+
+if website_urls is None or website_urls == "":
+    st.info("Please enter a website URL")
+
+else:
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, I am a bot. How can I help you?"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstores_from_urls(website_urls)
+
+    # user input
+    user_query = st.chat_input("Type your message here...")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+        
+       
+
+    # conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
---
+++ Pdf_Reader/app.py
@@ -0,0 +1,127 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from htmlTemplates import css, bot_template, user_template
+from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts.prompt import PromptTemplate
+
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    
+    llm = ChatOpenAI()
+
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    
+    template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+        Current conversation:
+        {history}
+        Human: {input}
+        AI Assistant:"""
+        
+    prompt = PromptTemplate(input_variables=["history", "input"], template=template)
+
+    
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        prompt = prompt,
+        memory=memory
+    )
+    
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+    
+    load_dotenv()
+    
+    st.set_page_config(page_title="Chat with multiple PDFs",
+                       page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
+
+
+if __name__ == '__main__':
+    main()
